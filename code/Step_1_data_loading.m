53f7b23a2950b0ae47d60465422407550ef737e1
Shijia Pan <shijia.pan@gmail.com>
2021-05-08 11:30:07 -0700

    update file

--- code/Step_1_data_loading.m
+++ code/Step_1_data_loading.m
@@ -2,11 +2,13 @@ clear all
 close all
 clc
 
-data = csvimport('./D1_fixed_front.csv');
+addpath('../functions');
+
+data = csvimport('./D1_fixed_bite.csv');
 dataM = cell2mat(data(2:end,:));
 D1_fix = dataM;
 
-data = csvimport('./D1_ill_front.csv');
+data = csvimport('./D1_ill_bite.csv');
 dataM = cell2mat(data(2:end,:));
 D1_ill = dataM;
 
@@ -14,13 +16,14 @@ data = csvimport('./D4_fixed_bite.csv');
 dataM = cell2mat(data(2:end,:));
 D4_fix = dataM;
 
-data = csvimport('./D4_ill_front.csv');
+data = csvimport('./D4_ill_bite.csv');
 dataM = cell2mat(data(2:end,:));
 D4_ill = dataM;
 
 save('D_data.mat','D1_ill','D1_fix','D4_ill','D4_fix');
 return;
-%%
+
+%% estimate sampling rate
 data = csvimport('./sda.csv');
 dataM = cell2mat(data(2:end,:));
 sample = dataM;
--- code/Step_3_feature_extraction.m
+++ code/Step_3_feature_extraction.m
@@ -5,15 +5,24 @@ clc;
 Fs = 2200;
 addpath('../functions/');
 
+load('../data/D5_fix_event.mat');
+load('../data/D5_ill_event.mat');
 load('../data/D4_fix_event.mat');
 load('../data/D4_ill_event.mat');
 
+
 [feature1, sigEnergy1] = eventFreqFeatureExtraction(D4_fix_event, Fs);
 [feature2, sigEnergy2] = eventFreqFeatureExtraction(D4_ill_event, Fs);
-species = [ones(19,1);zeros(18,1)];
+[feature3, sigEnergy3] = eventFreqFeatureExtraction(D5_fix_event, Fs);
+[feature4, sigEnergy4] = eventFreqFeatureExtraction(D5_ill_event, Fs);
+species = [ones(size(feature1,1),1);...
+            ones(size(feature2,1),1).*2;...
+            ones(size(feature3,1),1).*1;...
+            ones(size(feature4,1),1).*2];
 
-Y = tsne([feature1;feature2]);
+opts = statset('MaxIter',10000);
+Y = tsne([feature1;feature2;feature3;feature4],'Options',opts);
 figure;
-gscatter(Y(:,1),Y(:,2),species);
+gscatter(Y(:,1),Y(:,2),species,'br','xo');
 
 
